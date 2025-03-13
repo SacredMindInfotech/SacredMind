2401fbda7210c3e6567dd066a6c86f5628c2a477
akem <singhakem03@gmail.com>
2025-03-14 01:59:17 +0530

    script tag - pixel

--- frontend/src/MetaPixel.tsx
+++ frontend/src/MetaPixel.tsx
@@ -4,23 +4,38 @@ const MetaPixel = () => {
   useEffect(() => {
     // Facebook Pixel Script
     (function (f, b, e, v, n, t, s) {
+      //@ts-ignore
       if (f.fbq) return;
+      //@ts-ignore
       n = f.fbq = function () {
+        //@ts-ignore
         n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
       };
+      //@ts-ignore
       if (!f._fbq) f._fbq = n;
+      //@ts-ignore
       n.push = n;
+      //@ts-ignore
       n.loaded = !0;
+      //@ts-ignore
       n.version = "2.0";
+      //@ts-ignore
       n.queue = [];
+      //@ts-ignore
       t = b.createElement(e);
+      //@ts-ignore
       t.async = !0;
+      //@ts-ignore
       t.src = v;
+      //@ts-ignore
       s = b.getElementsByTagName(e)[0];
+      //@ts-ignore
       s.parentNode?.insertBefore(t, s);
     })(window, document, "script", "https://connect.facebook.net/en_US/fbevents.js");
 
+    //@ts-ignore
     window.fbq("init", "1349565019687622");
+    //@ts-ignore
     window.fbq("track", "PageView");
 
     // Inject noscript pixel tracking image dynamically
