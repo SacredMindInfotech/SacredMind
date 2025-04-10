ecc1d891dfcd292304288952f3255c73b692d57c
akem <singhakem03@gmail.com>
2025-04-10 11:36:33 +0530

    postcss-plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  css: {
+    postcss: {
+      plugins: [require('postcss-preset-env')]
+    }
+  },
   esbuild: {
     target: 'esnext',
     platform: 'node',
