6653f51220701d883927b4177414c9cd747a868f
akem <singhakem03@gmail.com>
2025-04-10 11:53:05 +0530

    bugs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,11 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  css: {
-    postcss: {
-      plugins: [require('postcss-preset-env')]
-    }
-  },
   esbuild: {
     target: 'esnext',
     platform: 'node',
